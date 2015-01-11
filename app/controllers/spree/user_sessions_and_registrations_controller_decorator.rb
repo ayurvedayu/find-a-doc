49069c6edb9cba79af0e35f5cc0ab035c663a243
Anton Mur <mibus32@gmail.com>
2015-01-11 21:20:00 +0400

    doctor redirect

---
+++ app/controllers/spree/user_sessions_and_registrations_controller_decorator.rb
@@ -0,0 +1,19 @@
+def after_sign_up_or_in_path resource
+	if resource.doctor? || resource.clinic?
+	  spree.user_profile_path
+	else
+	  super
+	end
+end
+
+Spree::UserSessionsController.class_eval do
+  def after_sign_in_path_for(resource)
+		after_sign_up_or_in_path resource  
+  end
+end
+
+Spree::UserRegistrationsController.class_eval do
+  def after_sign_up_path_for(resource)
+  	after_sign_up_or_in_path resource
+  end
+end
