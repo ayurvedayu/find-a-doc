dd6d8e2368c8d16e02c67d17812a62014779dd1c
Anton Mur <mibus32@gmail.com>
2015-01-12 15:13:45 +0400

    doctor as nested attribute to user

--- app/controllers/spree/user_controller_decorator.rb
+++ app/controllers/spree/user_controller_decorator.rb
@@ -2,5 +2,5 @@ Spree::UsersController.class_eval do
 
 end
 
-
-Spree::PermittedAttributes.user_attributes << [:make_role, :doctor_attributes]
+# adjust permitted params
+Spree::PermittedAttributes.user_attributes << [:make_role, doctor_attributes: [:clinic_id, :name, :description, :phone, :is_for_instant_booking, :id]]
