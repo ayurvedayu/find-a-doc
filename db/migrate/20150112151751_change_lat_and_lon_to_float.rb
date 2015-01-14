87119c21f248b5a4b2bfc624f1f77df290dfa5dd
Anton Mur <mibus32@gmail.com>
2015-01-14 21:50:30 +0400

    fix for postgres

--- db/migrate/20150112151751_change_lat_and_lon_to_float.rb
+++ db/migrate/20150112151751_change_lat_and_lon_to_float.rb
@@ -1,6 +1,6 @@
 class ChangeLatAndLonToFloat < ActiveRecord::Migration
   def change
-    change_column :spree_clinics, :latitude, :float 
-    change_column :spree_clinics, :longitude, :float 
+    change_column :spree_clinics, :latitude, 'float USING CAST(latitude AS float)'
+    change_column :spree_clinics, :longitude, 'float USING CAST(latitude AS float)'
   end
 end
