a613c83030fc42f8ece857e67c56d797dc3ad270
Anton Mur <mibus32@gmail.com>
2015-01-17 19:29:26 +0400

    better profiles

--- app/controllers/spree/user_sessions_and_registrations_controller_decorator.rb
+++ app/controllers/spree/user_sessions_and_registrations_controller_decorator.rb
@@ -2,18 +2,18 @@ def after_sign_up_or_in_path resource
 	if resource.doctor? || resource.clinic?
 	  spree.edit_account_path
 	else
-	  super
+	  false
 	end
 end
 
 Spree::UserSessionsController.class_eval do
   def after_sign_in_path_for(resource)
-		after_sign_up_or_in_path resource  
+		super unless after_sign_up_or_in_path resource
   end
 end
 
 Spree::UserRegistrationsController.class_eval do
   def after_sign_up_path_for(resource)
-  	after_sign_up_or_in_path resource
+  	super unless after_sign_up_or_in_path resource
   end
 end
